562679b11cc353e1955db99e8e9a15264a8d8fd8
Tùng Tạ Quang <tung.tq@teko.vn>
2020-12-10 17:29:52 +0700

    Add grpc gateway health

--- rpc/health/v1/health.pb.go
+++ rpc/health/v1/health.pb.go
@@ -6,6 +6,7 @@ package health
 import (
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	_ "google.golang.org/genproto/googleapis/api/annotations"
 	io "io"
 	math "math"
 	math_bits "math/bits"
@@ -22,24 +23,25 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type HealthRequest struct {
+// LiveRequest
+type LiveRequest struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *HealthRequest) Reset()         { *m = HealthRequest{} }
-func (m *HealthRequest) String() string { return proto.CompactTextString(m) }
-func (*HealthRequest) ProtoMessage()    {}
-func (*HealthRequest) Descriptor() ([]byte, []int) {
+func (m *LiveRequest) Reset()         { *m = LiveRequest{} }
+func (m *LiveRequest) String() string { return proto.CompactTextString(m) }
+func (*LiveRequest) ProtoMessage()    {}
+func (*LiveRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_fdbebe66dda7cb29, []int{0}
 }
-func (m *HealthRequest) XXX_Unmarshal(b []byte) error {
+func (m *LiveRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *HealthRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *LiveRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_HealthRequest.Marshal(b, m, deterministic)
+		return xxx_messageInfo_LiveRequest.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalToSizedBuffer(b)
@@ -49,36 +51,37 @@ func (m *HealthRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error
 		return b[:n], nil
 	}
 }
-func (m *HealthRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthRequest.Merge(m, src)
+func (m *LiveRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LiveRequest.Merge(m, src)
 }
-func (m *HealthRequest) XXX_Size() int {
+func (m *LiveRequest) XXX_Size() int {
 	return m.Size()
 }
-func (m *HealthRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthRequest.DiscardUnknown(m)
+func (m *LiveRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_LiveRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_HealthRequest proto.InternalMessageInfo
+var xxx_messageInfo_LiveRequest proto.InternalMessageInfo
 
-type HealthResponse struct {
+// LiveResponse
+type LiveResponse struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *HealthResponse) Reset()         { *m = HealthResponse{} }
-func (m *HealthResponse) String() string { return proto.CompactTextString(m) }
-func (*HealthResponse) ProtoMessage()    {}
-func (*HealthResponse) Descriptor() ([]byte, []int) {
+func (m *LiveResponse) Reset()         { *m = LiveResponse{} }
+func (m *LiveResponse) String() string { return proto.CompactTextString(m) }
+func (*LiveResponse) ProtoMessage()    {}
+func (*LiveResponse) Descriptor() ([]byte, []int) {
 	return fileDescriptor_fdbebe66dda7cb29, []int{1}
 }
-func (m *HealthResponse) XXX_Unmarshal(b []byte) error {
+func (m *LiveResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *HealthResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *LiveResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_HealthResponse.Marshal(b, m, deterministic)
+		return xxx_messageInfo_LiveResponse.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalToSizedBuffer(b)
@@ -88,38 +91,127 @@ func (m *HealthResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, erro
 		return b[:n], nil
 	}
 }
-func (m *HealthResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthResponse.Merge(m, src)
+func (m *LiveResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LiveResponse.Merge(m, src)
 }
-func (m *HealthResponse) XXX_Size() int {
+func (m *LiveResponse) XXX_Size() int {
 	return m.Size()
 }
-func (m *HealthResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthResponse.DiscardUnknown(m)
+func (m *LiveResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_LiveResponse.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_HealthResponse proto.InternalMessageInfo
+var xxx_messageInfo_LiveResponse proto.InternalMessageInfo
+
+// ReadyRequest
+type ReadyRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ReadyRequest) Reset()         { *m = ReadyRequest{} }
+func (m *ReadyRequest) String() string { return proto.CompactTextString(m) }
+func (*ReadyRequest) ProtoMessage()    {}
+func (*ReadyRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fdbebe66dda7cb29, []int{2}
+}
+func (m *ReadyRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ReadyRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ReadyRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ReadyRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ReadyRequest.Merge(m, src)
+}
+func (m *ReadyRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *ReadyRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_ReadyRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ReadyRequest proto.InternalMessageInfo
+
+// ReadyResponse
+type ReadyResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ReadyResponse) Reset()         { *m = ReadyResponse{} }
+func (m *ReadyResponse) String() string { return proto.CompactTextString(m) }
+func (*ReadyResponse) ProtoMessage()    {}
+func (*ReadyResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fdbebe66dda7cb29, []int{3}
+}
+func (m *ReadyResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *ReadyResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_ReadyResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *ReadyResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ReadyResponse.Merge(m, src)
+}
+func (m *ReadyResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *ReadyResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_ReadyResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ReadyResponse proto.InternalMessageInfo
 
 func init() {
-	proto.RegisterType((*HealthRequest)(nil), "health.v1.HealthRequest")
-	proto.RegisterType((*HealthResponse)(nil), "health.v1.HealthResponse")
+	proto.RegisterType((*LiveRequest)(nil), "health.v1.LiveRequest")
+	proto.RegisterType((*LiveResponse)(nil), "health.v1.LiveResponse")
+	proto.RegisterType((*ReadyRequest)(nil), "health.v1.ReadyRequest")
+	proto.RegisterType((*ReadyResponse)(nil), "health.v1.ReadyResponse")
 }
 
 func init() { proto.RegisterFile("health.proto", fileDescriptor_fdbebe66dda7cb29) }
 
 var fileDescriptor_fdbebe66dda7cb29 = []byte{
-	// 117 bytes of a gzipped FileDescriptorProto
+	// 239 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xc9, 0x48, 0x4d, 0xcc,
-	0x29, 0xc9, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0xf2, 0xca, 0x0c, 0x95, 0xf8,
-	0xb9, 0x78, 0x3d, 0xc0, 0x9c, 0xa0, 0xd4, 0xc2, 0xd2, 0xd4, 0xe2, 0x12, 0x25, 0x01, 0x2e, 0x3e,
-	0x98, 0x40, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x93, 0xd1, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e,
-	0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x42, 0x49, 0x7e, 0x4a,
-	0x7e, 0x62, 0x41, 0x81, 0x6e, 0x51, 0x41, 0xb2, 0x3e, 0x08, 0x43, 0xcc, 0xd3, 0x2f, 0x33, 0xb4,
-	0x86, 0xb0, 0x92, 0xd8, 0xc0, 0x16, 0x19, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xbf, 0x74, 0xf3,
-	0xdc, 0x78, 0x00, 0x00, 0x00,
+	0x29, 0xc9, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0xf2, 0xca, 0x0c, 0xa5, 0x64,
+	0xd2, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5, 0x13, 0x0b, 0x32, 0xf5, 0x13, 0xf3, 0xf2, 0xf2, 0x4b,
+	0x12, 0x4b, 0x32, 0xf3, 0xf3, 0x8a, 0x21, 0x0a, 0x95, 0x78, 0xb9, 0xb8, 0x7d, 0x32, 0xcb, 0x52,
+	0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x94, 0xf8, 0xb8, 0x78, 0x20, 0xdc, 0xe2, 0x82, 0xfc,
+	0xbc, 0xe2, 0x54, 0x10, 0x3f, 0x28, 0x35, 0x31, 0xa5, 0x12, 0x26, 0xcf, 0xcf, 0xc5, 0x0b, 0xe5,
+	0x43, 0x14, 0x18, 0x6d, 0x64, 0xe4, 0xe2, 0xf5, 0x00, 0xdb, 0x15, 0x9c, 0x5a, 0x54, 0x96, 0x99,
+	0x9c, 0x2a, 0xe4, 0xcb, 0xc5, 0x02, 0x32, 0x42, 0x48, 0x4c, 0x0f, 0xee, 0x06, 0x3d, 0x24, 0x2b,
+	0xa4, 0xc4, 0x31, 0xc4, 0xa1, 0x76, 0x89, 0x34, 0x5d, 0x7e, 0x32, 0x99, 0x89, 0x4f, 0x88, 0x47,
+	0x1f, 0xa2, 0x40, 0x3f, 0x07, 0x64, 0x4c, 0x20, 0x17, 0x2b, 0xd8, 0x46, 0x21, 0x64, 0x7d, 0xc8,
+	0x6e, 0x92, 0x92, 0xc0, 0x94, 0x80, 0x9a, 0x28, 0x0a, 0x36, 0x91, 0x5f, 0x88, 0x17, 0x66, 0x62,
+	0x11, 0x48, 0xda, 0xc9, 0xe8, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92,
+	0x63, 0x9c, 0xf1, 0x58, 0x8e, 0x21, 0x4a, 0xa1, 0x24, 0x3f, 0x25, 0x3f, 0xb1, 0xa0, 0x40, 0xb7,
+	0xa8, 0x20, 0x59, 0x1f, 0x84, 0xa1, 0xea, 0xcb, 0x0c, 0xad, 0x21, 0xac, 0x24, 0x36, 0x70, 0x70,
+	0x19, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x43, 0x23, 0xa6, 0x35, 0x67, 0x01, 0x00, 0x00,
 }
 
-func (m *HealthRequest) Marshal() (dAtA []byte, err error) {
+func (m *LiveRequest) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -129,12 +221,12 @@ func (m *HealthRequest) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *HealthRequest) MarshalTo(dAtA []byte) (int, error) {
+func (m *LiveRequest) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *HealthRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *LiveRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -146,7 +238,7 @@ func (m *HealthRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *HealthResponse) Marshal() (dAtA []byte, err error) {
+func (m *LiveResponse) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalToSizedBuffer(dAtA[:size])
@@ -156,12 +248,66 @@ func (m *HealthResponse) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *HealthResponse) MarshalTo(dAtA []byte) (int, error) {
+func (m *LiveResponse) MarshalTo(dAtA []byte) (int, error) {
 	size := m.Size()
 	return m.MarshalToSizedBuffer(dAtA[:size])
 }
 
-func (m *HealthResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+func (m *LiveResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ReadyRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ReadyRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ReadyRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *ReadyResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *ReadyResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *ReadyResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -184,7 +330,7 @@ func encodeVarintHealth(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *HealthRequest) Size() (n int) {
+func (m *LiveRequest) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -196,7 +342,31 @@ func (m *HealthRequest) Size() (n int) {
 	return n
 }
 
-func (m *HealthResponse) Size() (n int) {
+func (m *LiveResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *ReadyRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *ReadyResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -214,7 +384,7 @@ func sovHealth(x uint64) (n int) {
 func sozHealth(x uint64) (n int) {
 	return sovHealth(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-func (m *HealthRequest) Unmarshal(dAtA []byte) error {
+func (m *LiveRequest) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -237,10 +407,10 @@ func (m *HealthRequest) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: HealthRequest: wiretype end group for non-group")
+			return fmt.Errorf("proto: LiveRequest: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: HealthRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: LiveRequest: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
@@ -268,7 +438,7 @@ func (m *HealthRequest) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-func (m *HealthResponse) Unmarshal(dAtA []byte) error {
+func (m *LiveResponse) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -291,10 +461,118 @@ func (m *HealthResponse) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: HealthResponse: wiretype end group for non-group")
+			return fmt.Errorf("proto: LiveResponse: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: HealthResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: LiveResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipHealth(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthHealth
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthHealth
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ReadyRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowHealth
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ReadyRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ReadyRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipHealth(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if skippy < 0 {
+				return ErrInvalidLengthHealth
+			}
+			if (iNdEx + skippy) < 0 {
+				return ErrInvalidLengthHealth
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *ReadyResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowHealth
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: ReadyResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: ReadyResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
